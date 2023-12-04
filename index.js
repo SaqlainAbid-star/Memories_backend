fe779bef34ebbc97aa538bf647a7a2ddf474a39d
Saqlain Abid <jamalisaqlain7@gmail.com>
2023-12-04 13:21:32 +0500

    cors updated 5

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import postRoutes from './routes/posts.js'
-
-// initialize this app
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+var cors = require("cors");
 const app = express();
+import postRoutes from './routes/posts.js'
+const mongoose = require("./models/db/connection");
 
 
 app.use(express.json());
@@ -28,7 +27,11 @@ app.use(
 // setting up the body parser to properly send our requests
 // app.use(bodyParser.json({limit: '30mb',extended: true}));
 // app.use(bodyParser.urlencoded({limit: '30mb',extended: true}));
-// app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Sever is Running");
+  });
+  
 
 // we can use express middleware to connect routes to our application
 // First parameter: set up the starting path for all the routes
@@ -36,27 +39,7 @@ app.use(
 // it's reached by going to localhost:5000/posts because we added a prefix of posts to all routes
 app.use('/posts',postRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Sever is Running");
-  });
-  
 
-// connect our server application with a real database mongodb 
-// host our database on their cloud 
-// const Connection_URL = 'mongodb://127.0.0.1:27017/memoriesProject';
-const Connection_URL = 'mongodb+srv://Saqlain:Saqlain123@cluster0.oo31c.mongodb.net/?retryWrites=true&w=majority';
-
-const PORT =  5000;
-
-// mongoose to connect to our database
-mongoose.connect(Connection_URL,{useNewUrlParser: true, useUnifiedTopology: true })
-.then(()=> console.log("DB Connected."))
-.catch((error)=>console.log("Error: ",error.message));
-// mongoose.set('useFindAndModify', false);
-
-
-// we are connected to the database we can start creating routes for our backend application
-
-app.listen(PORT, () => {
+app.listen(5000, () => {
     console.log("Your server is running on 5000");
   });
